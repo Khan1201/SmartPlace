ef2d2d153c1591bb058036b99e83387c3370c24a
caa789 <caa789@naver.com>
2022-05-16 20:06:24 +0900

    SqlLiteHelper.java Add

---
+++ app/src/main/java/com/example/gradportfolio/Model/SqlLiteHelper.java
@@ -0,0 +1,31 @@
+package com.example.gradportfolio.Model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqlLiteHelper extends SQLiteOpenHelper {
+    public SqlLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE if not exists ShoppingBasket("
+                + "_id integer primary key autoincrement"
+                + ",url blob"
+                + ",brand_name text"
+                + ",product_name text"
+                + ",price text);";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE if exists ShoppingBasket";
+
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
